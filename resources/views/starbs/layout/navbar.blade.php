1edc6d8940bbb8e3548310363da247f5a958dba9
kevinanggaaa <kevin.anggahung@gmail.com>
2020-10-31 21:49:13 +0700

    finnish

--- resources/views/starbs/layout/navbar.blade.php
+++ resources/views/starbs/layout/navbar.blade.php
@@ -5,9 +5,10 @@
     <i class="fa fa-bars"></i>
   </button>
 
-  <div class="mr-0">
-    @livewire('navigation-dropdown')
-  </div>
+  <!-- Topbar Search -->
+  <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+  </form>
+  @livewire('navigation-dropdown')
 
 
 
