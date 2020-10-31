07d044eed5e9c8ede6e2edd2f3a04b151643433b
kevinanggaaa <kevin.anggahung@gmail.com>
2020-10-31 19:32:11 +0700

    test

--- resources/views/landingPage.blade.php
+++ resources/views/landingPage.blade.php
@@ -14,7 +14,7 @@
   <link href="{{ asset('/regna/assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Poppins:300,400,500,700" rel="stylesheet">
+  <link href="https:/fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Poppins:300,400,500,700" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
   <link href="{{ asset('/regna/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
